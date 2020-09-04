da128f73f8b18a90d0d04a229710e496d09d3140
Jake Haynes <47613806+ItsJakeHaynes@users.noreply.github.com>
2020-09-04 16:05:40 +0100

    Timers / color
    Added timers / percentages /

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -5,6 +5,8 @@ import { FormsModule } from '@angular/forms';
 import { HomePage } from './home.page';
 
 import { HomePageRoutingModule } from './home-routing.module';
+import { NgCircleProgressModule } from 'ng-circle-progress';
+import { RouterModule } from '@angular/router';
 
 
 @NgModule({
@@ -12,7 +14,24 @@ import { HomePageRoutingModule } from './home-routing.module';
     CommonModule,
     FormsModule,
     IonicModule,
-    HomePageRoutingModule
+    RouterModule.forChild([
+      {
+        path: '',
+        component: HomePage
+      }
+    ]),
+    NgCircleProgressModule.forRoot({
+      // set defaults here
+      radius: 100,
+      outerStrokeWidth: 16,
+      innerStrokeWidth: 8,
+      outerStrokeColor: "#78C000",
+      innerStrokeColor: "#C7E596",
+      animationDuration: 300,
+      animation: false,
+      responsive: true,
+      renderOnClick: false
+    })
   ],
   declarations: [HomePage]
 })
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { count } from 'console';
 
 @Component({
   selector: 'app-home',
@@ -7,6 +8,64 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  percent:number = 0;
+  radius:number = 100;
+  fullTime: any = '00:01:00';
+  timer: any = false;
+  progress: any = 0;
+  minutes: number = 1;
+  seconds: any = 30;
 
+  elapsed:any ={
+    h:'00',
+    m:'00',
+    s:'00'
+  }
+  overallTimer: any = false;
+
+  startTime() {
+
+    if(this.timer){
+      clearInterval(this.timer);
+    }
+
+    if(this.overallTimer) {
+      this.progressTimer();
+    }
+
+    this.timer=false;
+    this.percent = 0;
+    this.progress = 0;
+
+    let timeSplit = this.fullTime.split(':');
+    this.minutes = timeSplit[1];
+    this.seconds = timeSplit[2];
+    
+    let totalSeconds = Math.floor(this.minutes * 60) + parseInt(this.seconds);
+
+    this.timer = setInterval(() => {
+
+      if(this.percent == this.radius)
+        clearInterval(this.timer);
+
+      this.percent = Math.floor((this.progress / totalSeconds) * 100);
+      this.progress++;
+    }, 1000)
+  }
+
+  progressTimer() {
+    let countDownDate = new Date();
+
+    this.overallTimer = setInterval(() => {
+      let now = new Date().getTime();
+      let distance = now - countDownDate.getTime();
+
+      this.elapsed.h = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+      this.elapsed.m = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+      this.elapsed.s = Math.floor((distance % (1000 * 60)) / 1000);
+
+    })
+  }
 }
+
+
